b9d80962e83d8791c5a12de5f0144cd6c027e7a5
unknown <nikolajsok@gmail.com>
2024-04-20 16:19:25 +0200

    initial camera fragment logic

--- app/src/main/java/dh/ae/kesi/CameraFragment.kt
+++ app/src/main/java/dh/ae/kesi/CameraFragment.kt
@@ -1,59 +1,212 @@
 package dh.ae.kesi
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.location.Location
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
+import android.provider.Settings
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.material.snackbar.Snackbar
+import dh.ae.kesi.databinding.FragmentCameraBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CameraFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CameraFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var binding : FragmentCameraBinding
+
+    var picCount :Int = 0
+    var picArray = ArrayList<Bitmap>()
+
+    var lat :Double? = null
+    var long :Double? = null
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        getLocationPrem()
+        getLoc()
+
+
     }
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_camera, container, false)
+        binding = FragmentCameraBinding.inflate(inflater, container, false)
+
+        binding.takePic.setOnClickListener {
+            takePhoto()
+        }
+        //Log.d("CAMERA", picCount.toString())
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CameraFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CameraFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+
+    object CameraPermissionHelper {
+        private const val CAMERA_PERMISSION_CODE = 0
+        private const val CAMERA_PERMISSION = Manifest.permission.CAMERA
+
+        /** Check to see we have the necessary permissions for this app.  */
+        fun hasCameraPermission(activity: Activity): Boolean {
+            return ContextCompat.checkSelfPermission(activity, CAMERA_PERMISSION) == PackageManager.PERMISSION_GRANTED
+        }
+
+        /** Check to see we have the necessary permissions for this app, and ask for them if we don't.  */
+        fun requestCameraPermission(activity: Activity) {
+            ActivityCompat.requestPermissions(
+                activity, arrayOf(CAMERA_PERMISSION), CAMERA_PERMISSION_CODE)
+        }
+
+        /** Check to see if we need to show the rationale for this permission.  */
+        fun shouldShowRequestPermissionRationale(activity: Activity): Boolean {
+            return ActivityCompat.shouldShowRequestPermissionRationale(activity, CAMERA_PERMISSION)
+        }
+
+        /** Launch Application Setting to grant permission.  */
+        fun launchPermissionSettings(activity: Activity) {
+            val intent = Intent()
+            intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+            intent.data = Uri.fromParts("package", activity.packageName, null)
+            activity.startActivity(intent)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (!MainActivity.CameraPermissionHelper.hasCameraPermission(requireActivity())) {
+            Toast.makeText(activity, "Camera permission is needed to run this application", Toast.LENGTH_LONG)
+                .show()
+            if (!MainActivity.CameraPermissionHelper.shouldShowRequestPermissionRationale(requireActivity())) {
+                // Permission denied with checking "Do not ask again".
+                MainActivity.CameraPermissionHelper.launchPermissionSettings(requireActivity())
             }
+            activity?.finish()
+        }
+
+        activity?.recreate()
+    }
+
+
+    fun takePhoto(){
+        if (!MainActivity.CameraPermissionHelper.hasCameraPermission(requireActivity())) {
+            MainActivity.CameraPermissionHelper.requestCameraPermission(requireActivity())
+        }
+
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(requireActivity().packageManager)?.also {
+//                    imageUri = getImageUri(context, imageFile)
+//                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+                startActivityForResult(takePictureIntent, 1)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+
+            val image = data?.extras?.get("data") as Bitmap
+            picArray.add(image)
+            Log.d("CAMERA", image.toString())
+            Log.d("CAMERA", picCount.toString())
+
+            if(picCount < 5){
+                picCount++
+                takePhoto()
+            }else{
+                binding.firstLayout.visibility = View.GONE
+                binding.secondLayout.visibility = View.VISIBLE}
+
+            //binding.firstLayout.visibility = View.GONE
+            //binding.secondLayout.visibility = View.VISIBLE
+
+            /*image.apply {
+                view?.findViewById<ImageView>(R.id.memoImage)?.setImageBitmap(this)
+                // create rounded corners bitmap
+                view?.findViewById<ImageView>(R.id.memoImage)?.setImageBitmap(toRoundedCorners(8F))
+            }*/
+
+
+
+        }
+    }
+
+    fun getLocationPrem(){
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission
+                    .ACCESS_COARSE_LOCATION) || ActivityCompat.shouldShowRequestPermissionRationale (requireActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                Log.d("Debug", "snekbar")
+                Snackbar.make(
+                    binding.root,
+                    R.string.permission_location_rationale,
+                    Snackbar.LENGTH_INDEFINITE
+                ).setAction(R.string.ok) {
+                    ActivityCompat.requestPermissions(
+                        requireActivity(), arrayOf(
+                            Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        ),
+                        8
+                    )
+                }.show()
+            } else{
+                ActivityCompat.requestPermissions(
+                    requireActivity(), arrayOf(
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ),
+                    8
+                )
+            }
+            return
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    fun getLoc(){
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location : Location? ->
+                lat = location?.latitude
+                long = location?.longitude
+
+
+                //TODO do some check for if location os off
+            }
+    }
+
+
+
+    companion object {
+
     }
 }
--- app/src/main/java/dh/ae/kesi/MainActivity.kt
+++ app/src/main/java/dh/ae/kesi/MainActivity.kt
@@ -1,11 +1,90 @@
 package dh.ae.kesi
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.gms.location.FusedLocationProviderClient
+import dh.ae.kesi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private var fragmentTransaction : FragmentTransaction? = null
+
+    /** Helper to ask camera permission.  */
+    object CameraPermissionHelper {
+        private const val CAMERA_PERMISSION_CODE = 0
+        private const val CAMERA_PERMISSION = Manifest.permission.CAMERA
+
+        /** Check to see we have the necessary permissions for this app.  */
+        fun hasCameraPermission(activity: Activity): Boolean {
+            return ContextCompat.checkSelfPermission(activity, CAMERA_PERMISSION) == PackageManager.PERMISSION_GRANTED
+        }
+
+        /** Check to see we have the necessary permissions for this app, and ask for them if we don't.  */
+        fun requestCameraPermission(activity: Activity) {
+            ActivityCompat.requestPermissions(
+                activity, arrayOf(CAMERA_PERMISSION), CAMERA_PERMISSION_CODE)
+        }
+
+        /** Check to see if we need to show the rationale for this permission.  */
+        fun shouldShowRequestPermissionRationale(activity: Activity): Boolean {
+            return ActivityCompat.shouldShowRequestPermissionRationale(activity, CAMERA_PERMISSION)
+        }
+
+        /** Launch Application Setting to grant permission.  */
+        fun launchPermissionSettings(activity: Activity) {
+            val intent = Intent()
+            intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+            intent.data = Uri.fromParts("package", activity.packageName, null)
+            activity.startActivity(intent)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            Toast.makeText(this, "Camera permission is needed to run this application", Toast.LENGTH_LONG)
+                .show()
+            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
+                // Permission denied with checking "Do not ask again".
+                CameraPermissionHelper.launchPermissionSettings(this)
+            }
+            finish()
+        }
+
+        recreate()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //request camera permission if not granted
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            CameraPermissionHelper.requestCameraPermission(this)
+        }
+
+        //hide the action bar
+        supportActionBar?.hide()
+
+        //initialise camera fragment
+        fragmentTransaction = supportFragmentManager.beginTransaction()
+        val camFragment = CameraFragment()
+
+
+        fragmentTransaction?.add(R.id.fragmentFrame, camFragment)
+        fragmentTransaction?.commit()
+
     }
 }
